02b2fbd0dd17b4c1bc8877113cfc40b963b9550f
inecon <saturnua@gmail.com>
2016-01-17 21:12:22 +0200

    SAX

---
+++ MyParser.java
@@ -0,0 +1,57 @@
+package xml;
+
+import java.io.FileReader;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+public class MyParser extends DefaultHandler{
+	String result = "";
+	
+	public void printIndent(int indentSize) {
+	    for (int s = 0; s < indentSize; s++) {
+	      System.out.print(" ");
+	    }
+	  }
+	public static void main (String args[])throws Exception {
+		
+		XMLReader xr = XMLReaderFactory.createXMLReader();
+		MyParser handler = new MyParser();
+		xr.setContentHandler(handler);
+		xr.setErrorHandler(handler);
+		
+	    FileReader r = new FileReader("f:\\Обучение\\Java\\xml\\computerStore.xml");
+	    xr.parse(new InputSource(r));
+
+	}
+	
+	public MyParser() {
+		super();
+	}
+	
+	public void startDocument ()
+	{
+	System.out.println("Start document");
+	}
+	 
+	public void endDocument ()
+	{
+	System.out.println("End document");
+	}
+
+
+	public void startElement (String uri, String name, String qName, Attributes atts){
+		//System.out.println(qName) ;
+		for (int att = 0; att < atts.getLength(); att++) {
+		      String attName = atts.getQName(att);
+		      System.out.println(" " + attName + ": " + atts.getValue(attName));
+		    }
+	  }
+	
+	
+}
+
